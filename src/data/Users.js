6ac358302ed11d185dfc8f3bce19b22f1805c437
james <jamespulkaram@gmail.com>
2024-06-27 15:42:09 +0530

    Initial commit

---
+++ src/data/Users.js
@@ -0,0 +1,27 @@
+const users = [
+    {
+        id: 0,
+        email: 'nawal@gmail.com',
+        username: 'Nawal',
+        password: '123456',
+        settings: {
+            language: "English (US)",
+            currency: "USD",
+            subscribtion: "Basic"
+        }
+    },
+    {
+        id: 1,
+        email: 'Catherine@gmail.com',
+        username: 'Catherine',
+        password: '123456',
+        settings: {
+            language: "English (US)",
+            currency: "USD",
+            subscribtion: "Basic"
+        }
+
+    }
+];
+
+export default users;
